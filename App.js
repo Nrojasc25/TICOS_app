d1e3f6e619a3568146db0ebfc602e88be76c132c
Natalia Rojas <natalia.rojas@ufl.edu>
2024-10-07 16:35:55 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Events from './screens/Events';
+import Points from './screens/Points';
+import { Ionicons } from '@expo/vector-icons'; // Optional: for icons
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Events"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Events') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline'; // Change these icons as desired
+            } else if (route.name === 'Points') {
+              iconName = focused ? 'ios-star' : 'ios-star-outline'; // Change these icons as desired
+            }
+
+            // Return the icon component
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Events" component={Events} />
+        <Tab.Screen name="Points" component={Points} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
